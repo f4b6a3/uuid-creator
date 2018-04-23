d3d4e674b37d4f40f1fdad3cd5c610c400139b06
Fabio Lima <fabiolimace@gmail.com>
2018-04-23 10:47:47 -0300

    Created simple maven project.

---
+++ src/main/java/com/github/small/uuid/App.java
@@ -0,0 +1,13 @@
+package com.github.small.uuid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
