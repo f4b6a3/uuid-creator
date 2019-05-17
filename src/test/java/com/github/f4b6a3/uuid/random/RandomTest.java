3a1c3768049c22f09643ee33aee7c2d00ec6c468
Fabio Lima <fabiolimace@gmail.com>
2019-05-17 00:13:45 -0300

    Small changes

--- src/test/java/com/github/f4b6a3/uuid/random/RandomTest.java
+++ src/test/java/com/github/f4b6a3/uuid/random/RandomTest.java
@@ -11,20 +11,21 @@ import java.util.UUID;
 
 public class RandomTest {
 
+	private static final int DEFAULT_LOOP_LIMIT = 10_000;
+	
 	@Test
 	public void testGetRandomUuid_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			UUID uuid = UuidCreator.getRandom();
 			long msb = uuid.getMostSignificantBits();
 			long lsb = uuid.getLeastSignificantBits();
 			accumulator += Long.bitCount(msb) + Long.bitCount(lsb);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random UUUIDs is 63", average == 63);
 	}
@@ -32,17 +33,16 @@ public class RandomTest {
 	@Test
 	public void testGetFastRandomUuid_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			UUID uuid = UuidCreator.getFastRandom();
 			long msb = uuid.getMostSignificantBits();
 			long lsb = uuid.getLeastSignificantBits();
 			accumulator += Long.bitCount(msb) + Long.bitCount(lsb);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random UUUIDs is 63", average == 63);
 	}
@@ -50,15 +50,14 @@ public class RandomTest {
 	@Test
 	public void testRandomUtilNextLong_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			long value = RandomUtil.nextLong();
 			accumulator += Long.bitCount(value);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random long values is 32", average == 32);
 	}
@@ -66,17 +65,16 @@ public class RandomTest {
 	@Test
 	public void testXorshiftNextLong_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
 		XorshiftRandom random = new XorshiftRandom();
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			long value = random.nextLong();
 			accumulator += Long.bitCount(value);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random long values is 32", average == 32);
 	}
@@ -84,17 +82,16 @@ public class RandomTest {
 	@Test
 	public void testXorshiftStarNextLong_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
 		XorshiftStarRandom random = new XorshiftStarRandom();
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			long value = random.nextLong();
 			accumulator += Long.bitCount(value);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random long values is 32", average == 32);
 	}
@@ -102,17 +99,16 @@ public class RandomTest {
 	@Test
 	public void testXorshift128PlusNextLong_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
 		Xorshift128PlusRandom random = new Xorshift128PlusRandom();
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			long value = random.nextLong();
 			accumulator += Long.bitCount(value);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random long values is 32", average == 32);
 	}
@@ -120,17 +116,16 @@ public class RandomTest {
 	@Test
 	public void testXoroshiro128PlusNextLong_NaiveAverageBitCount() {
 		
-		double max = 10_000;
 		double accumulator = 0;
 		
 		Xoroshiro128PlusRandom random = new Xoroshiro128PlusRandom();
 		
-		for(int i = 0; i < max; i++) {
+		for(int i = 0; i < DEFAULT_LOOP_LIMIT; i++) {
 			long value = random.nextLong();
 			accumulator += Long.bitCount(value);
 		}
 		
-		double average = Math.round(accumulator / max);
+		double average = Math.round(accumulator / DEFAULT_LOOP_LIMIT);
 		
 		assertTrue("The average bit count expected for random long values is 32", average == 32);
 	}
