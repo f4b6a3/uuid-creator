4fdaa4d54704694c5e9fc736633cd67121707a44
Fabio Lima <fabiolimace@gmail.com>
2020-12-14 01:07:53 -0300

    Add license headers in the new codecs #30
    Add license headers to the new codecs.
    
    Add testcases for UuidValidator.

--- src/main/java/com/github/f4b6a3/uuid/util/UuidValidator.java
+++ src/main/java/com/github/f4b6a3/uuid/util/UuidValidator.java
@@ -44,8 +44,13 @@ public final class UuidValidator {
 		return uuid != null && uuid.length == 16;
 	}
 
-	// TODO: test
-	// TODO: javadoc
+	/**
+	 * Checks if the UUID byte array is valid.
+	 * 
+	 * @param uuid    a UUID byte array
+	 * @param version a version number
+	 * @return true if valid, false if invalid
+	 */
 	public static boolean isValid(final byte[] uuid, int version) {
 		return uuid != null && uuid.length == 16 && isVersion(uuid, version);
 	}
@@ -53,7 +58,7 @@ public final class UuidValidator {
 	/**
 	 * Checks if the UUID byte array is valid.
 	 * 
-	 * @param uuid a UUID
+	 * @param uuid a UUID byte array
 	 * @throws InvalidUuidException if invalid
 	 */
 	public static void validate(final byte[] uuid) {
@@ -62,8 +67,13 @@ public final class UuidValidator {
 		}
 	}
 
-	// TODO: test
-	// TODO: javadoc
+	/**
+	 * Checks if the UUID byte array is valid.
+	 * 
+	 * @param uuid    a UUID byte array
+	 * @param version a version number
+	 * @throws InvalidUuidException if invalid
+	 */
 	public static void validate(final byte[] uuid, int version) {
 		if (uuid == null || uuid.length != 16 || !isVersion(uuid, version)) {
 			throw new InvalidUuidException("Invalid UUID byte array.");
@@ -73,15 +83,6 @@ public final class UuidValidator {
 	/**
 	 * Checks if the UUID string is valid.
 	 * 
-	 * <pre>
-	 * Examples of accepted formats:
-	 * 
-	 * 12345678abcdabcdabcd123456789abcd       (32 hexadecimal chars, lower case and without hyphen)
-	 * 12345678ABCDABCDABCD123456789ABCD       (32 hexadecimal chars, UPPER CASE and without hyphen)
-	 * 12345678-abcd-abcd-abcd-123456789abcd   (36 hexadecimal chars, lower case and with hyphen)
-	 * 12345678-ABCD-ABCD-ABCD-123456789ABCD   (36 hexadecimal chars, UPPER CASE and with hyphen)
-	 * </pre>
-	 * 
 	 * @param uuid a UUID string
 	 * @return true if valid, false if invalid
 	 */
@@ -89,23 +90,19 @@ public final class UuidValidator {
 		return uuid != null && uuid.length() != 0 && isUuidString(uuid.toCharArray());
 	}
 
-	// TODO: javadoc
-	// TODO: test
+	/**
+	 * Checks if the UUID string is valid.
+	 * 
+	 * @param uuid    a UUID string
+	 * @param version a version number
+	 * @return true if valid, false if invalid
+	 */
 	public static boolean isValid(final String uuid, int version) {
 		return uuid != null && uuid.length() != 0 && isUuidString(uuid.toCharArray(), version);
 	}
 
 	/**
 	 * Checks if the UUID char array is valid.
-	 * 
-	 * <pre>
-	 * Examples of accepted formats:
-	 * 
-	 * 12345678abcdabcdabcd123456789abcd       (32 hexadecimal chars, lower case and without hyphen)
-	 * 12345678ABCDABCDABCD123456789ABCD       (32 hexadecimal chars, UPPER CASE and without hyphen)
-	 * 12345678-abcd-abcd-abcd-123456789abcd   (36 hexadecimal chars, lower case and with hyphen)
-	 * 12345678-ABCD-ABCD-ABCD-123456789ABCD   (36 hexadecimal chars, UPPER CASE and with hyphen)
-	 * </pre>
 	 * 
 	 * @param uuid a UUID char array
 	 * @return true if valid, false if invalid
@@ -114,8 +111,13 @@ public final class UuidValidator {
 		return uuid != null && uuid.length != 0 && isUuidString(uuid);
 	}
 
-	// TODO: javadoc
-	// TODO: test
+	/**
+	 * Checks if the UUID char array is valid.
+	 * 
+	 * @param uuid    a UUID char array
+	 * @param version a version number
+	 * @return true if valid, false if invalid
+	 */
 	public static boolean isValid(final char[] uuid, int version) {
 		return uuid != null && uuid.length != 0 && isUuidString(uuid, version);
 	}
@@ -132,8 +134,13 @@ public final class UuidValidator {
 		}
 	}
 
-	// TODO: javadoc
-	// TODO: test
+	/**
+	 * Checks if the UUID string is a valid.
+	 * 
+	 * @param uuid    a UUID string
+	 * @param version a version number
+	 * @throws InvalidUuidException if invalid
+	 */
 	public static void validate(final String uuid, int version) {
 		if (uuid == null || uuid.length() == 0 || !isUuidString(uuid.toCharArray(), version)) {
 			throwInvalidUuidException(uuid);
@@ -152,42 +159,17 @@ public final class UuidValidator {
 		}
 	}
 
-	// TODO: javadoc
-	// TODO: test
+	/**
+	 * Checks if the UUID char array is a valid.
+	 * 
+	 * @param uuid    a UUID char array
+	 * @param version a version number
+	 * @throws InvalidUuidException if invalid
+	 */
 	public static void validate(final char[] uuid, int version) {
 		if (uuid == null || uuid.length == 0 || !isUuidString(uuid, version)) {
 			throwInvalidUuidException(uuid);
 		}
-	}
-
-	private static void throwInvalidUuidException(char[] chars) {
-		String string = chars == null ? null : new String(chars);
-		throw new InvalidUuidException("Invalid UUID: " + string);
-	}
-
-	private static void throwInvalidUuidException(String string) {
-		throw new InvalidUuidException("Invalid UUID: " + string);
-	}
-
-	private static boolean isVersion(byte[] bytes, int version) {
-		boolean versionOk = ((version & ~0xf) == 0) && (((bytes[6] & 0xff) >>> 4) == version);
-		boolean variantOk = ((bytes[8] & 0xff) >>> 6) == 2; // RFC-4122
-		return versionOk && variantOk;
-	}
-
-	private static boolean isVersion(char[] chars, int version) {
-		final char[] lower = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-		final char[] upper = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-		boolean versionOk = (version & ~0xf) == 0 && chars[12] == lower[version] || chars[12] == upper[version];
-		boolean variantOk = chars[16] == '8' || chars[16] == '9' //
-				|| chars[16] == 'a' || chars[16] == 'b' || chars[16] == 'A' || chars[16] == 'B';
-		return versionOk && variantOk;
-	}
-
-	// TODO: javadoc
-	// TODO: test
-	protected static boolean isUuidString(final char[] chars, int version) {
-		return isVersion(chars, version) && isUuidString(chars);
 	}
 
 	/**
@@ -222,5 +204,65 @@ public final class UuidValidator {
 		}
 
 		return (c.length == 36 && hyphens == 4) || (c.length == 32 && hyphens == 0);
+	}
+
+	/**
+	 * Checks if the UUID char array in the standard format.
+	 * 
+	 * @param c       a char array
+	 * @param version a version number
+	 * @return true if valid, false if invalid
+	 */
+	protected static boolean isUuidString(final char[] chars, int version) {
+		return isVersion(chars, version) && isUuidString(chars);
+	}
+	
+	// TODO: test it individually 
+	protected static boolean isVersion(byte[] bytes, int version) {
+		boolean versionOk = ((version & ~0xf) == 0) && (((bytes[6] & 0xff) >>> 4) == version);
+		boolean variantOk = ((bytes[8] & 0xff) >>> 6) == 2; // RFC-4122
+		return versionOk && variantOk;
+	}
+
+	// TODO: test it individually 
+	protected static boolean isVersion(char[] chars, int version) {
+
+		// valid if between 0x0 and 0xf
+		if ((version & ~0xf) != 0) {
+			return false;
+		}
+
+		int ver = 0; // version index
+		int var = 0; // variant index
+
+		switch (chars.length) {
+		case 32: // without hyphen
+			ver = 12;
+			var = 16;
+			break;
+		case 36: // with hyphen
+			ver = 14;
+			var = 19;
+			break;
+		default:
+			return false;
+		}
+
+		final char[] lower = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		final char[] upper = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		boolean versionOk = (version & ~0xf) == 0 && chars[ver] == lower[version] || chars[ver] == upper[version];
+		boolean variantOk = chars[var] == '8' || chars[var] == '9' //
+				|| chars[var] == 'a' || chars[var] == 'b' || chars[var] == 'A' || chars[var] == 'B';
+
+		return versionOk && variantOk;
+	}
+
+	private static void throwInvalidUuidException(char[] chars) {
+		String string = chars == null ? null : new String(chars);
+		throw new InvalidUuidException("Invalid UUID: " + string);
+	}
+
+	private static void throwInvalidUuidException(String string) {
+		throw new InvalidUuidException("Invalid UUID: " + string);
 	}
 }
--- src/test/java/com/github/f4b6a3/uuid/util/UuidValidatorTest.java
+++ src/test/java/com/github/f4b6a3/uuid/util/UuidValidatorTest.java
@@ -13,6 +13,7 @@ public class UuidValidatorTest {
 
 	private static final int DEFAULT_LOOP_LIMIT = 10_000;
 
+	// TODO: test with version
 	@Test
 	public void testIsValidBytes() {
 
@@ -27,7 +28,7 @@ public class UuidValidatorTest {
 
 		uuid = new byte[17];
 		assertFalse("UUID byte array with length greater than 16 should be invalid .", UuidValidator.isValid(uuid));
-
+		
 		try {
 			uuid = null;
 			UuidValidator.validate(uuid);
@@ -93,6 +94,69 @@ public class UuidValidatorTest {
 		try {
 			uuid = null;
 			UuidValidator.validate(uuid);
+			fail();
+		} catch (InvalidUuidException e) {
+			// Success
+		}
+	}
+	
+	@Test
+	public void testIsValidStringWithVersion() {
+
+		int version = 4;
+		String uuid = null; // Null
+		assertFalse("Null UUID string should be invalid.", UuidValidator.isValid(uuid, version));
+
+		uuid = ""; // length: 0
+		assertFalse("UUID with empty string should be invalid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-abcd-ef0123456789"; // String length = 36
+		assertTrue("UUID with length equals to 36 should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-abcd-ef01-3456789"; // String length = 36, but an extra hyphen
+		assertFalse("UUID with length equals to 36 with an extra hyphen should be invalid.",
+				UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-abcddef0123456789"; // String length = 36, but a missing hyphen
+		assertFalse("UUID with length equals to 36 with a missing hyphen should be invalid.",
+				UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789ab4defabcdef0123456789"; // String length = 32
+		assertTrue("UUID with length equals to 32 should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789ab4defabcdef012345678"; // String length = 31
+		assertFalse("UUID with length equals to 31 should be invalid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789ab4defabcdef01234567899"; // String length = 33
+		assertFalse("UUID with length equals to 33 should be invalid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-abcd-ef0123456789"; // All lower case
+		assertTrue("UUID in lower case should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89AB-4DEF-ABCD-EF0123456789"; // All upper case
+		assertTrue("UUID in upper case should valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4DEF-abcd-EF0123456789"; // Mixed case
+		assertTrue("UUID in upper and lower case should valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789AB4DEFABCDEF0123456789"; // All upper case, without hyphen
+		assertTrue("UUID in upper case without hyphen should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789ab4defabcdef0123456789"; // All lower case, without hyphen
+		assertTrue("UUID in lower case without hyphen should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "0123456789ab4DEFabcdEF0123456789"; // mixed case, without hyphen
+		assertTrue("UUID in upper and lower case without hyphen should be valid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-abcd-SOPQRSTUVXYZ"; // String with non hexadecimal chars
+		assertFalse("UUID string with non hexadecimal chars should be invalid.", UuidValidator.isValid(uuid, version));
+
+		uuid = "01234567-89ab-4def-!@#$-ef0123456789"; // String with non alphanumeric chars
+		assertFalse("UUID string non alphanumeric chars should be invalid.", UuidValidator.isValid(uuid, version));
+
+		try {
+			uuid = null;
+			UuidValidator.validate(uuid, version);
 			fail();
 		} catch (InvalidUuidException e) {
 			// Success
