e64bfc38c9fa83dfbb3f52e77f25f0d961fd6762
Fabio Lima <fabiolimace@gmail.com>
2021-09-13 20:15:56 -0300

    Optimize uuid codecs more #47
    This is a cpu usage optimization.
    
    It was necesary to use `String.toUpperCase()` to get a UPPER CASE
    base-16 string.
    
    ```java
    String hexa = Base16Codec.INSTANCE.encode(uuid).toUpperCase();
    ```
    
    Now the user can choose a codec that encodes to UPPER CASE.
    
    ```java
    String hexa = Base16UCodec.INSTANCE.encode(uuid);
    ```

---
+++ src/main/java/com/github/f4b6a3/uuid/codec/base/Base32UCodec.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2018-2021 Fabio Lima
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.f4b6a3.uuid.codec.base;
+
+import com.github.f4b6a3.uuid.codec.base.function.Base32Decoder;
+import com.github.f4b6a3.uuid.codec.base.function.Base32Encoder;
+
+/**
+ * Codec for UPPER CASE base-32 as defined in RFC-4648.
+ * 
+ * It is case insensitive, so it decodes from lower and upper case, but encodes
+ * to UPPER CASE only.
+ * 
+ * This codec complies with RFC-4648, encoding a byte array sequentially. If you
+ * need a codec that encodes integers using the remainder operator (modulus),
+ * use the static factory {@link BaseNCodec#newInstance(BaseN)}.
+ * 
+ * See: https://tools.ietf.org/html/rfc4648
+ */
+public final class Base32UCodec extends BaseNCodec {
+
+	private static final BaseN BASE_N = new BaseN("A-Z2-7");
+
+	/**
+	 * A shared immutable instance.
+	 */
+	public static final Base32UCodec INSTANCE = new Base32UCodec();
+
+	public Base32UCodec() {
+		super(BASE_N, new Base32Encoder(BASE_N), new Base32Decoder(BASE_N));
+	}
+}
