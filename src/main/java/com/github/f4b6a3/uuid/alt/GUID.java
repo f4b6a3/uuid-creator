7524c6df1c17df8d6486554e61b8683aa9180099
Fabio Lima <fabiolimace@gmail.com>
2024-04-06 16:12:34 -0300

    chore

--- src/main/java/com/github/f4b6a3/uuid/alt/GUID.java
+++ src/main/java/com/github/f4b6a3/uuid/alt/GUID.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  * 
- * Copyright (c) 2018-2023 Fabio Lima
+ * Copyright (c) 2018-2024 Fabio Lima
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
